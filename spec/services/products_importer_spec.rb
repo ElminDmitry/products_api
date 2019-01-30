1192f78e8edc3046077edba1ecf3456e7e7304c7
ElminDmitry <elmin.mailbox@gmail.com>
2019-01-30 11:42:55 +0200

    Add mock file for specs

--- spec/services/products_importer_spec.rb
+++ spec/services/products_importer_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
 
+CSV_PATH = Rails.root.join('spec', 'data', 'MOCK_DATA.csv').freeze
+
 RSpec.describe ProductsImporter do
   describe 'Products importing' do
 
     it 'should parse and store products' do
-      expect { ProductsImporter.import }.to change { Product.count }
+      expect { ProductsImporter.import(CSV_PATH) }.to change { Product.count }.by(9)
     end
   end
 end
