294494114ad118620ee417f49343d1b5f8a88776
ElminDmitry <elmin.mailbox@gmail.com>
2019-01-30 11:26:50 +0200

    Add small controller test

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe ProductsController, type: :controller do
+  describe '#index' do
+    subject { get :index }
 
+    context 'return success' do
+      it { is_expected.to be_successful }
+    end
+  end
 end
